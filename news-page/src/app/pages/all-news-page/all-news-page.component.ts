898edc1697ee1a768ab4db71445a546ca2c6e99a
bervalim <bernardogvalim@gmail.com>
2024-05-21 19:48:05 -0300

    first-commit

---
+++ news-page/src/app/pages/all-news-page/all-news-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-news-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './all-news-page.component.html',
+  styleUrl: './all-news-page.component.scss'
+})
+export class AllNewsPageComponent {
+
+}
