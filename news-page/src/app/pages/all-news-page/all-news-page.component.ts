d4228f4d8561acd9dd82dc750f76ddf726eb7159
bervalim <bernardogvalim@gmail.com>
2024-05-22 10:19:32 -0300

    feat/login page structure

--- news-page/src/app/pages/all-news-page/all-news-page.component.ts
+++ news-page/src/app/pages/all-news-page/all-news-page.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/template/header/header.component';
 
 @Component({
   selector: 'app-all-news-page',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './all-news-page.component.html',
-  styleUrl: './all-news-page.component.scss'
+  styleUrl: './all-news-page.component.scss',
 })
-export class AllNewsPageComponent {
-
-}
+export class AllNewsPageComponent {}
--- news-page/src/app/pages/register-page/register-page.component.ts
+++ news-page/src/app/pages/register-page/register-page.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/template/header/header.component';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './register-page.component.html',
-  styleUrl: './register-page.component.scss'
+  styleUrl: './register-page.component.scss',
 })
-export class RegisterPageComponent {
-
-}
+export class RegisterPageComponent {}
