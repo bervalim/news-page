f26f4d2962eeee7dc9dc309fb43787b58001c6cb
bervalim <bernardogvalim@gmail.com>
2024-05-22 20:05:55 -0300

    feat/implementing visibility input password type on login form component

--- news-page/src/app/components/login-form/login-form.component.ts
+++ news-page/src/app/components/login-form/login-form.component.ts
@@ -1,25 +1,48 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, signal } from '@angular/core';
 import {
   FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { MatCommonModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [CommonModule, RouterLink, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    RouterLink,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatCommonModule,
+  ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss',
 })
 export class LoginFormComponent {
+  constructor(private changeDetectorRef: ChangeDetectorRef) {}
+
   loginForm = new FormGroup({
     email: new FormControl<string | null>(null, [Validators.required]),
     password: new FormControl<string | null>(null, [Validators.required]),
   });
+
+  readonly isPasswordHiddenSignal = signal<boolean>(true);
+
+  get isPasswordHidden() {
+    return this.isPasswordHiddenSignal();
+  }
+
+  togglePasswordVisibility(field: 'password') {
+    if (field === 'password') {
+      this.isPasswordHiddenSignal.set(!this.isPasswordHiddenSignal());
+    }
+    this.changeDetectorRef.markForCheck();
+  }
 
   get errors() {
     return {
