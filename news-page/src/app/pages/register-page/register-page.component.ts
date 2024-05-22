17ecf35a02c6fb54e7f17f06b8f6e9d9651e5427
bervalim <bernardogvalim@gmail.com>
2024-05-22 10:32:30 -0300

    feat/register page structure

--- news-page/src/app/pages/register-page/register-page.component.ts
+++ news-page/src/app/pages/register-page/register-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/template/header/header.component';
+import { RegisterFormComponent } from '../../components/register-form/register-form.component';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, RegisterFormComponent],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.scss',
 })
