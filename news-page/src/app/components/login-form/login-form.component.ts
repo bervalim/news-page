789b7e6b0b5002525b8350b1a51104bf94bd817d
bervalim <bernardogvalim@gmail.com>
2024-05-22 10:36:09 -0300

    feat/implementing router link on login and register pages

--- news-page/src/app/components/login-form/login-form.component.ts
+++ news-page/src/app/components/login-form/login-form.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './login-form.component.html',
-  styleUrl: './login-form.component.scss'
+  styleUrl: './login-form.component.scss',
 })
-export class LoginFormComponent {
-
-}
+export class LoginFormComponent {}
--- news-page/src/app/pages/register-page/register-page.component.ts
+++ news-page/src/app/pages/register-page/register-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/template/header/header.component';
 import { RegisterFormComponent } from '../../components/register-form/register-form.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [HeaderComponent, RegisterFormComponent],
+  imports: [HeaderComponent, RegisterFormComponent, RouterLink],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.scss',
 })
