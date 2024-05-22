b6591c5a5c7185766bdd5a549798b32b487be329
bervalim <bernardogvalim@gmail.com>
2024-05-22 13:00:27 -0300

    feat/comparasion betwenn passwords implemented

--- news-page/src/app/components/register-form/register-form.component.ts
+++ news-page/src/app/components/register-form/register-form.component.ts
@@ -1,12 +1,64 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+
+export const confirmPasswordValidator: ValidatorFn = (
+  control: AbstractControl
+): ValidationErrors | null => {
+  return control.value.password === control.value.confirmPassword
+    ? null
+    : { PasswordNoMatch: true };
+};
 
 @Component({
   selector: 'app-register-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register-form.component.html',
-  styleUrl: './register-form.component.scss'
+  styleUrls: ['./register-form.component.scss'],
 })
 export class RegisterFormComponent {
+  registerForm = new FormGroup(
+    {
+      name: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(80),
+      ]),
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email,
+        Validators.maxLength(45),
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern(/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])/),
+      ]),
+      confirmPassword: new FormControl(null, [Validators.required]),
+    },
+    { validators: [confirmPasswordValidator] }
+  );
 
+  get errors() {
+    return {
+      name: this.registerForm.get('name')?.errors,
+      email: this.registerForm.get('email')?.errors,
+      password: this.registerForm.get('password')?.errors,
+      confirmPassword: this.registerForm.get('confirmPassword')?.errors,
+    };
+  }
+
+  submitRegisterForm() {
+    if (this.registerForm.status === 'VALID') {
+      console.log(this.registerForm.value);
+    }
+  }
 }
